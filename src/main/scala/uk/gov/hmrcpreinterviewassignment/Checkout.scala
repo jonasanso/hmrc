9868cf9d09988e760e7073cddeb1a4cc5202fe59
Jonas Anso <jonas.anso@gmail.com>
2016-02-08 16:44:40 +0100

    Implemented step 1 with basic unit testing

--- src/main/scala/uk/gov/hmrcpreinterviewassignment/Checkout.scala
+++ src/main/scala/uk/gov/hmrcpreinterviewassignment/Checkout.scala
@@ -2,13 +2,27 @@ package uk.gov.hmrcpreinterviewassignment
 
 object Checkout {
 
-  def totalCost(items: List[String]) : Int = {
-    0
+  def totalCost(cart: List[String]) : Int = {
+    Item(cart).map(_.price).sum
   }
 
 }
 
 
+
+sealed abstract class Item(val price: Int)
+
+case object APPLE extends Item(60)
+case object ORANGE extends Item(25)
+
+object Item {
+  def apply(item: String) : Item = item match {
+    case "apple" => APPLE
+    case "orange" => ORANGE
+  }
+
+  def apply(items: List[String]): List[Item] = items.map(apply)
+}
 
 
 
--- src/test/scala/uk/gov/hmrcpreinterviewassignment/CheckoutSpec.scala
+++ src/test/scala/uk/gov/hmrcpreinterviewassignment/CheckoutSpec.scala
@@ -6,8 +6,24 @@ import org.specs2.mutable._
 class CheckoutSpec extends Specification {
 
   "Checkout calculate totalCost" should {
-    "be 0 for an empty basket" in {
+    "be 0 for an empty shopping cart" in {
       Checkout.totalCost(List()) must beEqualTo(0)
+    }
+
+    "be 60p for one apple" in {
+      Checkout.totalCost(List("apple")) must beEqualTo(60)
+    }
+
+    "be 25p for one orange" in {
+      Checkout.totalCost(List("orange")) must beEqualTo(25)
+    }
+
+    "be 180p for 3 apples" in {
+      Checkout.totalCost(List("apple", "apple", "apple")) must beEqualTo(180)
+    }
+
+    "be 205p for 3 aples and one orange" in {
+      Checkout.totalCost(List("apple", "apple", "orange", "apple")) must beEqualTo(205)
     }
 
   }
